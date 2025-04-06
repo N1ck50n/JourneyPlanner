8c7911f62024d5114fb15f9bdaa4ba4f3bfabee6
Nickson Tandel <tandeln@lancaster.ac.uk>
2025-04-06 10:44:17 +0100

    File reading and storing in an array.

---
+++ FileRead.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class FileRead extends Input
+{
+    public FileRead()
+    {
+        super();
+
+        ArrayList<String> startingLocations = new ArrayList<String>();
+        ArrayList<String> endingLocations = new ArrayList<String>();
+        ArrayList<String> type = new ArrayList<String>();
+        ArrayList<String> time = new ArrayList<String>();
+        
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader("Metrolink_times_linecolour.csv"));    
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null)
+            {
+                currentLine = reader.readLine();
+                String[] currentRow = currentLine.split(",");
+                startingLocations.add(currentRow[0]);
+                endingLocations.add(currentRow[1]);
+                type.add(currentRow[2]);
+                time.add(currentRow[3]);
+            }    
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error.");
+        }
+        
+    }
+
+}
